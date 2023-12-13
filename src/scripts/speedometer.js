d54377720471163f7e3dd0fa46363d9ce77e26ff
Thiago Luiz <thiagoluix21@gmail.com>
2023-12-13 06:00:55 -0300

    feat: :sparkles: LocalStorage implementation
    Functions were created so that every time a new race starts, it saves the data to LocalStorage using Date.now() as the key and storing the values there.

--- src/scripts/speedometer.js
+++ src/scripts/speedometer.js
@@ -3,10 +3,13 @@ const startBtn = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
 
 let watchID = null;
+let currentRide = null;
 startBtn.addEventListener("click", () => {
     if (watchID) return;
 
     const handleSuccess = (position) => {
+        addPosition(currentRide, position);
+        console.log(position);
         speedElement.innerHTML = position.coords.speed
             ? (position.coords.speed * 3.6).toFixed(1)
             : 0;
@@ -16,12 +19,12 @@ startBtn.addEventListener("click", () => {
         console.log(error);
     };
     const options = { enableHighAccuracy: true };
+    currentRide = createNewRide();
     watchID = navigator.geolocation.watchPosition(
         handleSuccess,
         handleError,
         options,
     );
-
     startBtn.classList.add("d-none");
     stopBtn.classList.remove("d-none");
 });
@@ -31,6 +34,8 @@ stopBtn.addEventListener("click", () => {
 
     navigator.geolocation.clearWatch(watchID);
     watchID = null;
+    updateStopTime(currentRide);
+    currentRide = null;
     startBtn.classList.remove("d-none");
     stopBtn.classList.add("d-none");
 });
---
+++ src/scripts/storage.js
@@ -0,0 +1,41 @@
+const createNewRide = () => {
+    const rideId = Date.now();
+    const rideRecord = {
+        data: [],
+        startTime: rideId,
+        stopTime: null,
+    };
+
+    saveRideRecord(rideId, rideRecord);
+    return rideId;
+};
+
+const getRideRecord = (rideId) => {
+    return JSON.parse(localStorage.getItem(rideId));
+};
+
+const saveRideRecord = (rideId, rideRecord) => {
+    localStorage.setItem(rideId, JSON.stringify(rideRecord));
+};
+
+const addPosition = (rideId, position) => {
+    const rideRecord = getRideRecord(rideId);
+    const newData = {
+        accuracy: position.coords.accuracy,
+        altitude: position.coords.altitude,
+        altitudeAccuracy: position.coords.altitudeAccuracy,
+        heading: position.coords.heading,
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        speed: position.coords.speed,
+        timestamp: position.timestamp,
+    };
+    rideRecord.data.push(newData);
+    saveRideRecord(rideId, rideRecord);
+};
+
+const updateStopTime = (rideId) => {
+    const rideRecord = getRideRecord(rideId);
+    rideRecord.stopTime = Date.now();
+    saveRideRecord(rideId, rideRecord);
+};
