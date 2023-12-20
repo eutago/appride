e49486ee3c279b7f2f3d468b11d9eeabd9fa2e1e
Thiago Luiz <thiagoluix21@gmail.com>
2023-12-20 20:45:16 -0300

    style: :lipstick: Racing page styling
    The items were styled in Javascript with the bootstrap classes and the bug of changing the order of items on the race page was also fixed.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -85,34 +85,49 @@ allRides.forEach(async ([id, value]) => {
     const ride = JSON.parse(value);
     ride.id = id;
 
+    const itemElement = document.createElement("li");
+    itemElement.id = ride.id;
+    itemElement.className = "d-flex p-1 align-items-center gap-3 shadow-sm ";
+
+    rideListElement.appendChild(itemElement);
+
     const firstPosition = ride.data[0];
     const firstLocationData = await getLocationData(
         firstPosition.latitude,
         firstPosition.longitude,
     );
 
-    const itemElement = document.createElement("li");
-    itemElement.id = ride.id;
+    const mapElement = document.createElement("div");
+    mapElement.style = "width: 100px; height: 100px;";
+    mapElement.className = "bg-secondary rounded-4";
+
+    const dataElement = document.createElement("div");
+    dataElement.className = "flex-fill d-flex flex-column";
 
     const cityDiv = document.createElement("div");
     cityDiv.innerHTML = `${firstLocationData.city} - ${firstLocationData.countryCode}`;
+    cityDiv.className = "text-primary mb-2";
 
     const maxSpeedDiv = document.createElement("div");
     maxSpeedDiv.innerHTML = `Max speed: ${getMaxSpeed(ride.data)} Km/h`;
+    maxSpeedDiv.className = "h5";
 
     const distanceDiv = document.createElement("div");
     distanceDiv.innerText = `Distance: ${getDistance(ride.data)} Km`;
 
     const durationDiv = document.createElement("div");
-    durationDiv.innerText = getDuration(ride);
+    durationDiv.innerText = `Duration: ${getDuration(ride)}`;
 
     const dateDiv = document.createElement("div");
     dateDiv.innerText = getStartDate(ride);
+    dateDiv.className = "text-secondary mt-2";
 
-    itemElement.appendChild(cityDiv);
-    itemElement.appendChild(maxSpeedDiv);
-    itemElement.appendChild(distanceDiv);
-    itemElement.appendChild(durationDiv);
-    itemElement.appendChild(dateDiv);
-    rideListElement.appendChild(itemElement);
+    dataElement.appendChild(cityDiv);
+    dataElement.appendChild(maxSpeedDiv);
+    dataElement.appendChild(distanceDiv);
+    dataElement.appendChild(durationDiv);
+    dataElement.appendChild(dateDiv);
+
+    itemElement.appendChild(mapElement);
+    itemElement.appendChild(dataElement);
 });
